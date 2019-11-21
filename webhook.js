936cd5f96849566ca533abdd05951c84c1f5b1b8
fengjinlong <18618166564@163.com>
2019-11-21 23:34:11 +0800

    date

--- webhook.js
+++ webhook.js
@@ -1,10 +1,16 @@
 let http = require('http')
+let crypto = require('crypto')
+let SECRET = '123456'
+function sign (body) {
+  return `sha1=` + CaretPosition.creatHmac('sha1', SECRET).update(body).digest('hex')
+}
 let server = http.createServer((req, res) => {
-  // res.setHeader('Access-Control-Allow-Origin', '*')
-  // Access-Control-Allow-Origin
   console.log('webhook 🍺')
   if (req.method === 'POST' && req.url == '/webhook') {
-    // res.end(JSON.stringify(users))
+    let buffers = []
+    req.on('data', function (buffer) {
+      buffers.push(buffer)
+    })
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify({ok: true}))
   } else {
