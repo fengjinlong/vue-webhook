ca38156dd8f051290be9f078eea4ab112563c6f1
fengjinlong <18618166564@163.com>
2019-11-21 23:59:23 +0800

    date

--- vue-back.sh
+++ vue-back.sh
@@ -6,9 +6,9 @@ git reset --hard origin/master
 echo "拉取新代码"
 git pull origin master
 echo "开始执行构建"
-docker build -t vue-back .
+docker build -t vue-back:1.0 .
 echo "停止旧容器并删除旧容器"
 docker stop vue-back-container
 docker rm vue-back-container
 echo "启动新容器"
-docker container run -p 3000:3000 vue-back-container -d vue-back
+docker container run -p 3000:3000 vue-back-container -d vue-back:1.0
