e8a3bc5131941e791660bce6718909263a582ed4
fengjinlong <18618166564@163.com>
2019-11-22 21:32:56 +0800

    date

--- vue-back.sh
+++ vue-back.sh
@@ -12,4 +12,4 @@ echo "停止旧容器并删除旧容器"
 docker stop vue-back-container
 docker rm vue-back-container
 echo "启动新容器"
-docker container run -p 3000:3000 vue-back-container -d vue-back:1.0
+docker container run -p 3000:3000 --name vue-back-container -d vue-back:1.0
