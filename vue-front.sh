1f2bbd47ef730347ac770613ae0eaf64df3c3fcd
fengjinlong <18618166564@163.com>
2019-11-22 22:05:55 +0800

    date

---
+++ vue-front.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+WORK_PATH='/usr/projects/vue-front'
+cd $WORK_PATH
+echo "清除老代码"
+git reset --hard origin/master
+git clean -f
+echo "拉取新代码"
+git pull origin master
+npm run build
+echo "开始执行构建"
+docker build -t vue-front:1.0 .
+echo "停止旧容器并删除旧容器"
+docker stop vue-front-container
+docker rm vue-front-container
+echo "启动新容器"
+docker container run -p 80:80 --name vue-front-container -d vue-front:1.0
